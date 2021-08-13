d61d354bca287afd045cbf532c71f14f49f6f196
Anton Zavodchikov <terisbackno@gmail.com>
2021-08-13 07:56:11 +0500

    Oof

--- src/routes/auth/callback.svelte
+++ src/routes/auth/callback.svelte
@@ -3,20 +3,9 @@
 	 * @type {import('@sveltejs/kit').Load}
 	 */
 	export async function load({ page, fetch, session, context }) {
-		const url = `/blog/${page.params.slug}.json`;
-		const res = await fetch(url);
-
-		if (res.ok) {
-			return {
-				props: {
-					article: await res.json()
-				}
-			};
-		}
-
 		return {
-			status: res.status,
-			error: new Error(`Could not load ${url}`)
+			status: 200,
+			error: new Error(`Could not load`)
 		};
 	}
 </script>
