b04091d6e00b7feac1eb2f8f47cc1142f6a4ed05
Anton Zavodchikov <terisbackno@gmail.com>
2021-08-14 16:32:54 +0500

    In process of jams feed

--- src/routes/jams.svelte
+++ src/routes/jams.svelte
@@ -1,8 +1,12 @@
 <script>
 	export let jams = [
 		{
-			title: 'Some shit',
-			image: 'https://via.placeholder.com/160x90.png',
+			title: 'KOJIMA-ДЖЕМ на Russian Gamedev',
+			teaser:
+				'Админы копили с обедов, воровали, попрошайничали на конфэ джиди (э фэйерли комьюнити оф гейм девелопмент) и всё-таки накопили на джем!',
+			timestamp: '05 мая 2020',
+			preview: 'https://via.placeholder.com/160x90.png',
+			previewDescription: 'Анонс джема весеннего сезона 2020 года',
 			href: '/jam/123'
 		}
 	];
@@ -13,11 +17,20 @@
 </svelte:head>
 
 {#each jams as jam}
-	<a href={jam.href}>
-		<figure>
-			<img src={jam.image} alt="" />
-			<figcaption />
+	<a role="article" class="jam" href={jam.href}>
+		<!-- 16x9 image -->
+		<figure class="jam__image">
+			<img src={jam.preview} alt={jam.previewDescription} />
 		</figure>
-		{jam.title}</a
-	>
+		<h1 class="jam__title">{jam.title}</h1>
+		<p class="jam__teaser">{jam.description}</p>
+	</a>
 {/each}
+
+<style>
+	.jam {
+		display: flex;
+		flex-direction: column;
+		background-color: var(--secondary-color);
+	}
+</style>
