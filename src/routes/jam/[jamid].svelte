70e436b5981602f9e82008e0bc1f4956b4347796
Anton Zavodchikov <terisbackno@gmail.com>
2021-08-15 05:36:29 +0500

    Return test page

---
+++ src/routes/jam/[jamid].svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page, fetch, session, context }) {
+		return {
+			props: {
+				jamid: page.params.jamid
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let jamid;
+</script>
+
+<h1>{jamid}</h1>
