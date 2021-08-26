73e83fcb9a0e4255f793f40bfe223532242c5dbd
Anton Zavodchikov <terisbackno@gmail.com>
2021-08-26 06:58:44 +0500

    Add jams styles and header

--- src/routes/jams.svelte
+++ src/routes/jams.svelte
@@ -20,18 +20,23 @@
 	</div>
 </div>
 
-{#each jams as jam}
-	<a role="article" class="jam" title={jam.title} href={href(jam)}>
-		<div class="badges">
-			<span class="badge"> ЛЕТО 2021 </span>
-		</div>
-		<img src={jam.thumbnail} alt={jam.thumbnailAlt} />
-		<div class="jam__info">
-			<h1 class="title">{jam.title}</h1>
-			<p class="teaser">{jam.teaser}</p>
-		</div>
-	</a>
-{/each}
+<div class="jams">
+	<h3>Джемы</h3>
+	<div class="jams__list">
+		{#each jams.slice(1, jams.length) as jam}
+			<a role="article" class="jam" title={jam.title} href={href(jam)}>
+				<div class="badges">
+					<span class="badge"> ЛЕТО 2021 </span>
+				</div>
+				<img src={jam.thumbnail} alt={jam.thumbnailAlt} />
+				<div class="jam__info">
+					<h1 class="title">{jam.title}</h1>
+					<p class="teaser">{jam.teaser}</p>
+				</div>
+			</a>
+		{/each}
+	</div>
+</div>
 
 <style>
 	.banner {
@@ -57,6 +62,7 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
+		gap: 2rem;
 	}
 
 	.banner__info .info {
@@ -78,6 +84,62 @@
 		font-weight: 500;
 		line-height: 1.2rem;
 		color: var(--pure-white);
+	}
+
+	.jams {
+		display: flex;
+		flex-direction: column;
+		gap: 1.2rem;
+	}
+
+	.jams__list {
+		display: flex;
+		flex-wrap: wrap;
+		column-gap: 2.4rem;
+		row-gap: 1.6rem;
+	}
+
+	.jam {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		flex-basis: 15rem;
+		flex-grow: 1;
+		gap: 0.8rem;
+		border-radius: 0.4rem;
+		padding: 0.8rem;
+		overflow: hidden;
+		text-decoration: none;
+		background-color: var(--secondary-color);
+	}
+
+	.jam img {
+		margin: 0;
+		object-fit: cover;
+		width: 100%;
+		aspect-ratio: 10 / 6;
+		border-radius: 0.2rem;
+	}
+
+	.badges {
+		position: absolute;
+		top: 0;
+		right: 0;
+		margin: 1.5rem 1.2rem;
+		display: flex;
+		justify-content: flex-end;
+		color: var(--pure-white);
+	}
+
+	.badge {
+		display: block;
+		padding: 0.1rem 0.4rem;
+		font-size: 0.5rem;
+		font-weight: 500;
+		line-height: 0.8rem;
+		letter-spacing: 0.025rem;
+		border-radius: 2em;
+		background-color: var(--secondary-background);
 	}
 
 	.title,
