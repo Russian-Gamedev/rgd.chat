d1dbdab7032c0457edf61be758213002a5005ff8
Anton Zavodchikov <terisbackno@gmail.com>
2021-08-26 06:58:20 +0500

    Trim down jams titles

--- src/lib/jams.ts
+++ src/lib/jams.ts
@@ -7,7 +7,7 @@ export function href(jam: Jam): string {
 export const jams: Jam[] = [
   {
     id: 'wizard-jam',
-    title: 'Wizard Jam — Тема: Вжух и ты петух',
+    title: 'Wizard Jam',
     teaser:
       'У админов наступила ностальгия новогодних джемов, ведь 2020 мы пропустили, пусть вьюга станет нашим путеводом!',
     thumbnail: 'https://xshxhmntnugvfztasqwt.supabase.in/storage/v1/object/public/images/placeholder-banner.jpg',
@@ -15,7 +15,7 @@ export const jams: Jam[] = [
   },
   {
     id: 'army-jam',
-    title: 'Армейский джем — Тема: Война это искуство',
+    title: 'Армейский джем',
     teaser:
       'Беларус, локальный мем, и просто ненавистник тумбочек - Даня. В честь его отхода в армию этот джем!',
     thumbnail: 'https://img.youtube.com/vi/oklfZebbDsU/maxresdefault.jpg',
@@ -23,7 +23,7 @@ export const jams: Jam[] = [
   },
   {
     id: 'letniy-jam-2020',
-    title: 'Летний Джем 2020 — Тема: Сам себе враг',
+    title: 'Летний Джем 2020',
     teaser:
       'Привыкшие к новой реальности и даже уже немного отдохнувшие админы решили, что расслаблятся рано и пора ебошить джем!',
     thumbnail: 'https://img.youtube.com/vi/imZXyNb1UIM/maxresdefault.jpg',
@@ -31,7 +31,7 @@ export const jams: Jam[] = [
   },
   {
     id: 'karantin-jam',
-    title: 'Карантин Джем — Тема: Все вверх дном',
+    title: 'Карантин Джем',
     teaser:
       'Внезапно ёбанул карантин и кризис, но гд\'шники не стали унывать и стартанули новый перевёрнутый джем!',
     thumbnail: 'https://img.youtube.com/vi/lcv0BvLuMdE/maxresdefault.jpg',
@@ -39,7 +39,7 @@ export const jams: Jam[] = [
   },
   {
     id: 'kojima-jam',
-    title: 'KOJIMA-ДЖЕМ — Тема: HIDEOKOJIMA GAME',
+    title: 'KOJIMA-ДЖЕМ',
     teaser:
       'Админы копили с обедов, воровали, попрошайничали на конфэ джиди (э фэйерли комьюнити оф гейм девелопмент) и всё-таки накопили на джем!',
     thumbnail: 'https://img.youtube.com/vi/f5w1S53cvQU/maxresdefault.jpg',
