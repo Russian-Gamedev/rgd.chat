0e3be92d9986e01c2ac7e5632d7a6a53005a6db3
Anton Zavodchikov <terisbackno@gmail.com>
2021-08-26 12:05:56 +0500

    Change adapter to node, we are going to self host

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 
 	kit: {
 		target: '#root',
-		adapter: vercel()
+		adapter: node()
 	}
 };
 
