3158316b15ec86ddbdc70542c38767057da096ec
Anton Zavodchikov <terisbackno@gmail.com>
2021-09-04 15:59:40 +0500

    Delete unused routes in mvp

--- src/routes/blogs.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import MetaTags from '$lib/components/MetaTags.svelte';
-</script>
-
-<MetaTags noindex={true} nofollow={true} title="Блоги" />
-
-<h3>Блоги</h3>
--- src/routes/games.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import MetaTags from '$lib/components/MetaTags.svelte';
-</script>
-
-<MetaTags noindex={true} nofollow={true} title="Джемы" />
-
-Finally, some good fucking games.
