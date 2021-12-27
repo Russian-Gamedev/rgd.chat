1ee9ea4da39068c8ac4097dbde8aef106d5e9e19
Anton Zavodchikov <terisbackno@gmail.com>
2021-12-27 14:10:39 +0500

    add footer links, update social links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,9 +5,11 @@
 	import SocialYt from "$lib/icons/SocialYT.svelte";
 	import ExternalLink from "$lib/icons/ExternalLink.svelte";
 	import { stats } from "$lib/stores/stats";
+	import Kringga from "$lib/icons/Kringga.svelte";
+	import Github from "$lib/icons/Github.svelte";
 </script>
 
-<main>
+<main class="h-full flex flex-col">
 	<h1>Russian Gamedev — Discord сообщество</h1>
 	<p>
 		Обитель разработчиков игр, где вы услышите экспертное мнение по поводу своих
@@ -35,22 +37,7 @@
 		<ExternalLink />
 	</a>
 	<TertiaryHeader>Полезные ссылки</TertiaryHeader>
-	<div class="flex gap-12 gap-y-8">
-		<a
-			class="p-4 w-[19rem] flex flex-col items-start rounded-lg bg-black"
-			rel="external"
-			href="https://vk.com/rgd_discord"
-		>
-			<div
-				class="p-2.5 w-10 h-10 flex justify-center items-center text-white bg-primary rounded-xl"
-			>
-				<SocialVk />
-			</div>
-			<div class="mt-4">
-				<div class="text-card font-bold text-general">ВКонтакте</div>
-				<div class="mt-1.5 text-card">Сообщество с мемасиками</div>
-			</div>
-		</a>
+	<div class="mb-12 flex gap-12 gap-y-8 flex-wrap">
 		<a
 			class="p-4 w-[19rem] flex flex-col items-start rounded-lg bg-black"
 			rel="external"
@@ -69,7 +56,7 @@
 		<a
 			class="p-4 w-[19rem] flex flex-col items-start rounded-lg bg-black"
 			rel="external"
-			href="https://vk.com/rgd_discord"
+			href="https://youtu.be/bDBhfamPtvo"
 		>
 			<div
 				class="p-2.5 w-10 h-10 flex justify-center items-center text-white bg-primary rounded-xl"
@@ -80,6 +67,38 @@
 				<div class="text-card font-bold text-general">Последний джем</div>
 				<div class="mt-1.5 text-card">35 игр, 5 часов прохождения</div>
 			</div>
+		</a>
+		<a
+			class="p-4 w-[19rem] flex flex-col items-start rounded-lg bg-black"
+			rel="external"
+			href="https://vk.com/game_maker"
+		>
+			<div
+				class="p-2.5 w-10 h-10 flex justify-center items-center text-white bg-primary rounded-xl"
+			>
+				<SocialVk />
+			</div>
+			<div class="mt-4">
+				<div class="text-card font-bold text-general">ВКонтакте</div>
+				<div class="mt-1.5 text-card">Сообщество по Game Maker</div>
+			</div>
+		</a>
+	</div>
+
+	<div class="mt-auto flex gap-6 text-general text-badge">
+		<a
+			class="py-0.5 px-2 flex items-center rounded-full bg-black"
+			rel="external"
+			href="https://vk.com/kringga"
+		>
+			<Kringga />
+		</a>
+		<a
+			class="py-0.5 px-2 flex items-center rounded-full bg-black"
+			rel="external"
+			href="https://github.com/Russian-Gamedev/rgd.chat"
+		>
+			<Github />
 		</a>
 	</div>
 </main>
