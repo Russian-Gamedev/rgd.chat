9b655eb8467a5ef6bc731b4062d3ff35e6d1e12f
Anton Zavodchikov <terisbackno@gmail.com>
2021-12-29 07:10:08 +0500

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,22 @@
+import mdsvexConfig from "./mdsvex.config.js";
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [preprocess({})],
+  preprocess: [preprocess({}), mdsvex(mdsvexConfig)],
 
   kit: {
     adapter: adapter(),
     target: "#biba",
   },
+
+  plugins: [mdsvex],
 };
 
 export default config;
