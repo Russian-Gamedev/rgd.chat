49b67efa77b004268befc6ad1cdecd356abfce1f
Anton Zavodchikov <terisbackno@gmail.com>
2021-12-29 13:05:50 +0500

    almost done with kojima-jam

--- src/routes/jams/[slug].svelte
+++ src/routes/jams/[slug].svelte
@@ -24,17 +24,53 @@
 	export let jam;
 </script>
 
+<div class="flex xl:flex-row flex-col gap-7">
+	<iframe
+		class="h-[21rem] aspect-video rounded-lg bg-control"
+		src={jam.stream}
+		title="Запись джема"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+	/>
+	<div class="flex flex-col gap-6">
+		<div>
+			<span class="font-bold text-lg text-primary">
+				{jam.season}
+			</span>
+			<h2 class="m-0 mt-3 mb-2">
+				{jam.title}
+			</h2>
+		</div>
+		<div>
+			<h3 class="m-0">Призовой фонд</h3>
+			<p class="m-0 mt-1">
+				{#each jam.prize.split("\n") as line}
+					{line} <br />
+				{/each}
+			</p>
+		</div>
+		<div>
+			<h3 class="m-0">Дата проведения</h3>
+			<p class="m-0 mt-1">
+				{jam.start}
+				{jam.end}
+			</p>
+		</div>
+	</div>
+</div>
+
 {@html jam.html}
 
 {#if jam.games}
 	<TertiaryHeader>Игры ({jam.games.length})</TertiaryHeader>
 
-	<div class="flex flex-wrap gap-12 gap-y-8">
+	<div class="grid xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-1 gap-12 gap-y-8">
 		{#each jam.games as game}
 			<div class="relative p-4 grow-0 flex flex-col gap-4 rounded-lg bg-black">
 				{#if game.image}
 					<img
-						class="w-[17rem] aspect-video rounded"
+						class="aspect-video object-cover rounded"
 						alt=""
 						src={`/src/jams/${jam.slug}/${game.image}`}
 					/>
@@ -59,3 +95,13 @@
 		{/each}
 	</div>
 {/if}
+
+<style lang="postcss">
+	:global(li) {
+		@apply list-disc list-inside;
+	}
+
+	:global(li::marker) {
+		@apply text-primary;
+	}
+</style>
