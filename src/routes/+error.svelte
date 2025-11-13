9eb5a7292b72eeba51e91dc5d28b279b92c57ee5
DamirLut <lutfrahmanovdamir3@gmail.com>
2025-11-14 01:35:51 +0500

    feat: :construction: 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+	import NotFoundImage from '$lib/assets/clown_by_Jacob.png';
+</script>
+
+<main>
+	<h1>Такой страницы не нет</h1>
+	<p>Похоже, вы заблудились. Попробуйте вернуться на главную страницу.</p>
+	<img src={NotFoundImage} alt="Здесь ничего нет, как и актива на этом сайте. Made by Jacob." />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: 2rem;
+	}
+</style>
