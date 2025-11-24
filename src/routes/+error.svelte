425090f9a0d5bb84f858fa3a3014903fb3f1a7f7
DamirLut <lutfrahmanovdamir3@gmail.com>
2025-11-24 20:31:35 +0500

    fix: :bug: improve error handling for 404 pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,17 @@
 <script>
 	import NotFoundImage from '$lib/assets/clown_by_Jacob.png';
+	import { page } from '$app/state';
 </script>
 
 <main>
-	<h1>Такой страницы нет</h1>
-	<p>Похоже, вы заблудились. Попробуйте вернуться на главную страницу.</p>
+	{#if page.status === 404}
+		<h1>Такой страницы нет</h1>
+		<p>Похоже, вы заблудились. Попробуйте вернуться на главную страницу.</p>
+	{:else}
+		<p>Упс! Что-то пошло не так. Попробуйте обновить страницу или вернуться позже.</p>
+		<h4>{page.error?.message}</h4>
+	{/if}
+
 	<img src={NotFoundImage} alt="Здесь ничего нет, как и актива на этом сайте. Made by Jacob." />
 </main>
 
