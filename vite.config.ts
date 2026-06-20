f8b133ec9f95d96523652b9903e41e101aedcc40
DamirLut <lutfrahmanovdamir3@gmail.com>
2026-06-20 20:02:22 +0500

    feat: add API proxy to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: '0.0.0.0',
-		allowedHosts: true
-	}
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, '');
+	return {
+		plugins: [sveltekit()],
+		server: {
+			host: '0.0.0.0',
+			port: 5173,
+			allowedHosts: true,
+			strictPort: true,
+			proxy: {
+				'/api': {
+					target: env.VITE_API_TARGET_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, '')
+				}
+			}
+		}
+	};
 });
