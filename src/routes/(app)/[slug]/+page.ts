75767fc83f51538d02ecda593e4915db6d7f31a1
DamirLut <lutfrahmanovdamir3@gmail.com>
2026-07-05 23:21:01 +0500

    feat: enable SSR and prerendering for user profile pages

--- src/routes/(app)/[slug]/+page.ts
+++ src/routes/(app)/[slug]/+page.ts
@@ -6,6 +6,15 @@ import { createApi } from '$lib/api/api';
 
 import type { PageLoad } from './$types';
 
+export const ssr = true;
+export const prerender = true;
+
+export const entries = async () => {
+	const api = createApi({ fetch });
+	const patrons = await api.getPatrons();
+	return patrons.map((patron) => ({ slug: patron.user.username }));
+};
+
 export const load: PageLoad = async ({ params, depends, fetch }) => {
 	depends('user:profile');
 
