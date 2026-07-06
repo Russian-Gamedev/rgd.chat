aa9217867749b3118d2b8816fa43405d21b8aa86
DamirLut <lutfrahmanovdamir3@gmail.com>
2026-07-06 19:34:50 +0500

    refactor: make logout icon use currentColor

--- src/lib/assets/icons/logout.svelte
+++ src/lib/assets/icons/logout.svelte
@@ -7,21 +7,21 @@
 >
   <path
     d="M4.5 10.5H2.5C2.23478 10.5 1.98043 10.3946 1.79289 10.2071C1.60536 10.0196 1.5 9.76522 1.5 9.5V2.5C1.5 2.23478 1.60536 1.98043 1.79289 1.79289C1.98043 1.60536 2.23478 1.5 2.5 1.5H4.5"
-    stroke="#FEFEFE"
+    stroke="currentColor"
     stroke-width="0.75"
     stroke-linecap="round"
     stroke-linejoin="round"
   />
   <path
     d="M8 8.5L10.5 6L8 3.5"
-    stroke="#FEFEFE"
+    stroke="currentColor"
     stroke-width="0.75"
     stroke-linecap="round"
     stroke-linejoin="round"
   />
   <path
     d="M10.5 6H4.5"
-    stroke="#FEFEFE"
+    stroke="currentColor"
     stroke-width="0.75"
     stroke-linecap="round"
     stroke-linejoin="round"
